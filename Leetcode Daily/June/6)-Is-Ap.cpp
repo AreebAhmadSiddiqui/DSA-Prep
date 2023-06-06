c3daaa5bdbd88a9ec4d960b186eef29f1f045e70
Areeb Ahmad Siddiqui <sareebahmad@gmail.com>
2023-06-06 10:08:19 +0530

    Added Leetcode Daily Jun 6

---
+++ Leetcode Daily/June/6)-Is-Ap.cpp
@@ -0,0 +1,23 @@
+1502. Can Make Arithmetic Progression From Sequence
+
+A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+    
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
+        
+        int iniDiff=arr[1]-arr[0];
+        
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            int diff=arr[i+1]-arr[i];
+            if(diff!=iniDiff) return false;
+        }
+        return true;
+    }
+};
